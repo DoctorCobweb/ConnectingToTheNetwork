d0ffc75027a56a56de0100d39e2af2678230bc3b
Andre Trosky <andre@spinninghalf.com.au>
2012-11-29 15:35:57 +1100

    added content to Services and Contact tabs. Wiring it all up then fixing layout, graphic design and content afterwards.

--- src/au/com/spinninghalf/connectingtothenetwork/ContactFragmentOne.java
+++ src/au/com/spinninghalf/connectingtothenetwork/ContactFragmentOne.java
@@ -1,17 +1,25 @@
 package au.com.spinninghalf.connectingtothenetwork;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.actionbarsherlock.app.SherlockFragment;
 
 public class ContactFragmentOne extends SherlockFragment {
 	private final static String TAG = "ContactFragmentOne";
+	public final static String SPINNING_HALF_FACEBOOK_URI = "http://www.facebook.com/pages/Spinning-Half/227917577236619";
+	public final static String SPINNING_HALF_TWITTER_URI = "https://twitter.com/spinninghalf";
+	public final static String SPINNING_HALF_YOUTUBE_URI = "http://www.youtube.com/user/spinninghalfstudios?feature=mhee";
+	public final static String SPINNING_HALF_WEBSITE_URI = "http://www.spinninghalf.com.au";
 	
 	@Override
 	public void onAttach(Activity activity) {
@@ -41,5 +49,49 @@ public class ContactFragmentOne extends SherlockFragment {
 		super.onStart();
 		
 		Log.i(TAG, "in onStart()");
+		
+		//find all the button references
+		Button facebookButton = (Button) getActivity().findViewById(R.id.contactFacebookButton);
+		Button twitterButton = (Button) getActivity().findViewById(R.id.contactTwitterButton);
+		Button youtubeButton = (Button) getActivity().findViewById(R.id.contactYoutubeButton);
+		Button spinningHalfButton = (Button) getActivity().findViewById(R.id.contactSpinningHalfButton);
+		
+		//assign listeners to each button
+		facebookButton.setOnClickListener(facebookButtonListener);
+		twitterButton.setOnClickListener(twitterButtonListener);
+		youtubeButton.setOnClickListener(youtubeButtonListener);
+		spinningHalfButton.setOnClickListener(spinningHalfButtonListener);
 	}
+	
+	public OnClickListener facebookButtonListener = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(ContactFragmentOne.SPINNING_HALF_FACEBOOK_URI));
+			startActivity(intent);
+		}
+	};
+	
+	public OnClickListener twitterButtonListener = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(ContactFragmentOne.SPINNING_HALF_TWITTER_URI));
+			startActivity(intent);
+		}
+	};
+	
+	public OnClickListener youtubeButtonListener = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(ContactFragmentOne.SPINNING_HALF_YOUTUBE_URI));
+			startActivity(intent);
+		}
+	};
+	
+	public OnClickListener spinningHalfButtonListener = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(ContactFragmentOne.SPINNING_HALF_WEBSITE_URI));
+			startActivity(intent);
+		}
+	};
 }
--- src/au/com/spinninghalf/connectingtothenetwork/ServicesPhotographyActivity.java
+++ src/au/com/spinninghalf/connectingtothenetwork/ServicesPhotographyActivity.java
@@ -2,12 +2,15 @@ package au.com.spinninghalf.connectingtothenetwork;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class ServicesPhotographyActivity extends Activity {
+	public static final String BRENTON_FORD_URL = "https://www.facebook.com/brentonfordphotography";
+	public static final String LEIKOGRAPHY_URL = "https://www.facebook.com/leikography";
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -28,7 +31,7 @@ public class ServicesPhotographyActivity extends Activity {
 		
 		@Override
 		public void onClick(View v) {
-			Intent intent = new Intent(ServicesPhotographyActivity.this, ServicesPhotographyBrentonFordActivity.class);
+			Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(ServicesPhotographyActivity.BRENTON_FORD_URL));
 			startActivity(intent);
 		}
 	};
@@ -37,7 +40,7 @@ public class ServicesPhotographyActivity extends Activity {
 		
 		@Override
 		public void onClick(View v) {
-			Intent intent = new Intent(ServicesPhotographyActivity.this, ServicesPhotographyLeikographyActivity.class);
+			Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(ServicesPhotographyActivity.LEIKOGRAPHY_URL));
 			startActivity(intent);
 		}
 	};
