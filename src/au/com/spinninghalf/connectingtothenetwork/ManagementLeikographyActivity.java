f00bdb19d21a40ea8c9a6858199027baca1fc920
Andre Trosky <andre@spinninghalf.com.au>
2013-01-03 09:38:24 +1100

    added content, image updates.

--- src/au/com/spinninghalf/connectingtothenetwork/ManagementLeikographyActivity.java
+++ src/au/com/spinninghalf/connectingtothenetwork/ManagementLeikographyActivity.java
@@ -4,17 +4,22 @@ import com.google.analytics.tracking.android.EasyTracker;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
 import android.widget.GridView;
 
 public class ManagementLeikographyActivity extends Activity {
 	public final static String TAG = "ManagementLeikographyActivity";
 	public final static String ARG_SELECTED_IMAGE = "au.com.spinninghalf.connectingtothenetwork.ManagementLeikographyActivity.selectedimage";
 	public final static String ARG_SELECTED_IMAGE_POSITION = "au.com.spinninghalf.connectingtothenetwork.ManagementLeikographyActivity.selectedposition";
+	public final static String LEIKOGRAPHY_FACEBOOK_URI = "https://www.facebook.com/leikography";
+		
 	
 	// references to our images
     public Integer[] mThumbIds = {
@@ -35,6 +40,9 @@ public class ManagementLeikographyActivity extends Activity {
 		Log.i(TAG, "in onStart()");
 		
 		EasyTracker.getInstance().activityStart(this); // Add this method.
+		
+		Button leikographyButton = (Button) findViewById(R.id.managementLeikographyFacebook);
+		leikographyButton.setOnClickListener(leikographyButtonListener);
 		
 		GridView gridview = (GridView) findViewById(R.id.managementLeikographyGridView);
         gridview.setAdapter(new ManagementLeikographyImageAdapter(this));
@@ -65,5 +73,14 @@ public class ManagementLeikographyActivity extends Activity {
 		
 		EasyTracker.getInstance().activityStop(this); // Add this method.
 	}
+	
+	public OnClickListener leikographyButtonListener = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(ManagementLeikographyActivity.LEIKOGRAPHY_FACEBOOK_URI));
+			startActivity(intent);
+		}
+		
+	};
 
 }
